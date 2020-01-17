6b2a200cbb5f01f62901d14dcb5ffab40d13601b
jyshen2019 <jsbiz2018@gmail.com>
2020-01-16 21:29:18 -0800

    Initila commit

---
+++ insertion_sort.py
@@ -0,0 +1,18 @@
+"""Insertion Sort
+>>> ll = [23, 11, 45, 88, 20, 19, 18, 17, 99, 1]
+>>> print(insertion_sort(ll))
+[1, 11, 17, 18, 19, 20, 23, 45, 88, 99]
+
+"""
+import doctest
+
+def insertion_sort(ll):
+    for i in range(len(ll)):
+        j = i
+        while j > 0 and ll[j] < ll[j - 1]:
+            ll[j], ll[j - 1] = ll[j - 1], ll[j]
+            j -= 1
+    return ll
+
+if __name__ == "__main__":
+    doctest.testmod()
